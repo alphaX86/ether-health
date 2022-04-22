8c423dd24b11b9db88d45c5e904b9433330d7548
Aadhitya A <aadhitya864@gmail.com>
2022-04-22 15:00:11 +0530

    Replace ganache with Alchemy API
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- etx-heal/pages/index.js
+++ etx-heal/pages/index.js
@@ -4,17 +4,11 @@ import styles from '../styles/Home.module.css'
 
 function test() {
   var Web3 = require('web3');
-  var provider = 'http://127.0.0.1:7545';
+  var provider = 'https://eth-mainnet.alchemyapi.io/v2/g5rpUpvN8EVa4q0r3trp-7HhAeAYP8e5';
   var web3Provider = new Web3.providers.HttpProvider(provider);
   var web3 = new Web3(web3Provider);
   web3.eth.getBlockNumber().then((result) => {
     console.log("Latest Ethereum Block is ",result);
-  });
-  web3.eth.isMining().then((result) => {
-    console.log("Mining status is ",result);
-  });
-  web3.eth.getHashrate().then((result) => {
-    console.log("Hashrate is ",result);
   });
   web3.eth.getGasPrice().then((result)=> {
     console.log("Gas Price is ",result);
