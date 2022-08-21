3d4312cbe299c8fd987f4a4c6de5a044c30a47be
Aadhitya A <aadhitya864@gmail.com>
2022-08-21 20:26:02 +0530

    Update solidity contract

--- hms/models/contracts/idContract.sol
+++ hms/models/contracts/idContract.sol
@@ -2,65 +2,65 @@
 pragma solidity >=0.6.1;
 pragma experimental ABIEncoderV2;
 contract idContract {
-	bytes32 public dataId;
-	uint public numberOfRecords = 0;
-	bytes32[] public recordsList;
+	bytes32 public _dataId;
+	uint public _numberOfRecords = 0;
+	bytes32[] public _recordsList;
 
 	event dataAdded(string dat);
 
 	struct ID_Contr {
-		string payload;
-		uint listPointer;
+		string _payload;
+		uint _listPointer;
 	}
 
 	mapping(bytes32 => ID_Contr) public facts;
 
 	function isRecord(bytes32 recordAddress) public view returns (bool isRec) {
-		if(recordsList.length == 0) return false;
-		return (recordsList[facts[recordAddress].listPointer] == recordAddress);
+		if(_recordsList.length == 0) return false;
+		return (_recordsList[facts[recordAddress]._listPointer] == recordAddress);
 	}
 
 	function getRecordCount() public view returns (uint recCount){
-		return recordsList.length;
+		return _recordsList.length;
 	}
 
-	function addRecord(string memory payload, bytes32 ID) public returns (bool success) {
+	function addRecord(string memory _payload, bytes32 ID) public returns (bool success) {
 	if(isRecord(ID)) revert('record with this id already exists');
-		facts[ID].payload = payload;
-		recordsList.push(ID);
-		facts[ID].listPointer = recordsList.length - 1;
-		numberOfRecords++;
+		facts[ID]._payload = _payload;
+		_recordsList.push(ID);
+		facts[ID]._listPointer = _recordsList.length - 1;
+		_numberOfRecords++;
 		return (true);
 	}
 
-	function getRecord(bytes32 id) public view returns (string memory payload){
+	function getRecord(bytes32 id) public view returns (string memory _payload){
 	if(!isRecord(id)) revert('record with this id does not exist');
-		return (facts[id].payload);
+		return (facts[id]._payload);
 	}
 
-	function updateRecord(bytes32 id, string memory payload) public returns (bool success){
+	function updateRecord(bytes32 id, string memory _payload) public returns (bool success){
 		if(!isRecord(id)) revert('record with this id does not exist');
-		facts[id].payload = payload;
+		facts[id]._payload = _payload;
 		return (true);
 	}
 
-	function getAllRecords() public view returns (string[] memory payloads) {
-		string[] memory payloadss = new string[](numberOfRecords);
-		for (uint i = 0; i < numberOfRecords; i++) {
-			ID_Contr storage fact = facts[recordsList[i]];
-			payloadss[i] = fact.payload;
+	function getAllRecords() public view returns (string[] memory _payloads) {
+		string[] memory __payloadss = new string[](_numberOfRecords);
+		for (uint i = 0; i < _numberOfRecords; i++) {
+			ID_Contr storage _fact = facts[_recordsList[i]];
+			__payloadss[i] = _fact._payload;
 		}
-		return (payloadss);
+		return (__payloadss);
 	}
 
 function deleteRecord(bytes32 id) public returns (bool success) {
     if(!isRecord(id)) revert('record with this id does not exist');
-    uint rowToDelete = facts[id].listPointer;
-    bytes32 keyToMove = recordsList[recordsList.length-1];
-    recordsList[rowToDelete] = keyToMove;
-    facts[keyToMove].listPointer = rowToDelete;
-    recordsList.pop();
-    numberOfRecords--;
+    uint _rowToDelete = facts[id]._listPointer;
+    bytes32 _keyToMove = _recordsList[_recordsList.length-1];
+    _recordsList[_rowToDelete] = _keyToMove;
+    facts[_keyToMove]._listPointer = _rowToDelete;
+    _recordsList.pop();
+    _numberOfRecords--;
     return (true);
   }
 
